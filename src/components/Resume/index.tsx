5f0246312c4e7389b2944fd1a9c70a99027724b8
JackLins <juliocesar_19@hotmail.com.br>
2022-09-13 22:00:56 -0300

    type error solved

--- src/components/Resume/index.tsx
+++ src/components/Resume/index.tsx
@@ -3,15 +3,15 @@ import * as k from './styles'
 type Props ={
     title:string;
     value:number;
-    color ? : string;
+    color?: string;
 }
 
 
-export const Resume = ({title, value,color}:Props)=>{
+export const Resume = ({title, value, color}:Props)=>{
     return( 
         <k.Container>
             <k.Title>{title}</k.Title>
             <k.Info color={color}>R$ {value}</k.Info>
         </k.Container>
     )
-} 
+}  
--- src/components/Resume/styles.ts
+++ src/components/Resume/styles.ts
@@ -10,8 +10,8 @@ font-weight: bold;
 color: #888;
 margin-bottom: 5px
 `
-export const Info = styled.div<{color:string}>`
+export const Info = styled.div<{color?:string}>`
 text-align: center;
-font-weight: bold;
+font-weight: bold; 
 color: ${props => props.color ?? '#000'}
 `
